9c9450bafe4c9cb37f9678a2242dea283e2bc2aa
datagutten <datagutten@datagutten.net>
2021-05-21 15:46:54 +0200

    Add classes to init

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
 from .utils.build_lua_table import build_lua_table
 from .utils.build_lua_table import build_lua_list
 from .utils import WoWBuildUtils
+from .utils.Wowhead import Wowhead
+from .utils.WoWTables import WoWTables
