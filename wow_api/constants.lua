c43d6938aaebcf146131600e0b15a596abed5b2d
datagutten <datagutten@datagutten.net>
2022-04-01 20:01:32 +0200

    Add game version constants

--- wow_api/constants.lua
+++ wow_api/constants.lua
@@ -10,6 +10,21 @@ EXPANSION_NAME8 = "Shadowlands"
 
 ZONE = "Zone"
 
+--https://wowpedia.fandom.com/wiki/WOW_PROJECT_ID
+_G.WOW_PROJECT_MAINLINE = 1
+_G.WOW_PROJECT_CLASSIC = 2
+_G.WOW_PROJECT_BURNING_CRUSADE_CLASSIC = 5
+
+if os.getenv('GAME_VERSION') == 'classic' then
+    _G.WOW_PROJECT_ID = _G.WOW_PROJECT_CLASSIC
+elseif os.getenv('GAME_VERSION') == 'bcc' then
+    _G.WOW_PROJECT_ID = _G.WOW_PROJECT_BURNING_CRUSADE_CLASSIC
+elseif os.getenv('GAME_VERSION') == 'retail' then
+    _G.WOW_PROJECT_ID = _G.WOW_PROJECT_MAINLINE
+else
+    error('Environment variable GAME_VERSION is not set or has invalid value')
+end
+
 if os.getenv('GAME_VERSION') == 'bcc' then
     MAX_PLAYER_LEVEL = 70
 else
