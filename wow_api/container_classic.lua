54b0ee5a0f04ea689f9f13e75665eeebf14395c6
datagutten <datagutten@datagutten.net>
2022-11-20 12:57:13 +0100

    GetContainerItemInfo should return multiple values, not a table

--- wow_api/container_classic.lua
+++ wow_api/container_classic.lua
@@ -3,17 +3,16 @@ function GetContainerNumSlots(slot)
 end
 
 function GetContainerItemInfo(containerIndex, slotIndex)
-    return {
-        134414, --icon
-        1, --itemCount
-        false, --locked
-        1, --quality
-        false, --readable
-        false, --lootable
-        "|cffffffff|Hitem:6948::::::::42:::::|h[Hearthstone]|h|r", --itemLink
-        false, --isFiltered
-        true, --noValue
-        6948, --itemId
-        true, --isBound
-    }
+    return
+    134414, --icon
+    1, --itemCount
+    false, --locked
+    1, --quality
+    false, --readable
+    false, --lootable
+    "|cffffffff|Hitem:6948::::::::42:::::|h[Hearthstone]|h|r", --itemLink
+    false, --isFiltered
+    true, --noValue
+    6948, --itemId
+    true --isBound
 end
