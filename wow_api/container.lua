231db156078d0e699fa0253e14a74f0b61314ef1
datagutten <datagutten@datagutten.net>
2022-11-20 11:25:05 +0100

    Add container API dummies

---
+++ wow_api/container.lua
@@ -0,0 +1,21 @@
+_G.C_Container = {}
+
+function _G.C_Container.GetContainerNumSlots(slot)
+    return 30
+end
+
+function _G.C_Container.GetContainerItemInfo(containerIndex, slotIndex)
+    return {
+        hasLoot = false,
+        hyperlink = "|cffffffff|Hitem:6948::::::::49:253:::::|h[Hearthstone]|h|r",
+        iconFileID = 134414,
+        hasNoValue = true,
+        isLocked = false,
+        itemID = 6948,
+        isBound = true,
+        stackCount = 1,
+        isFiltered = false,
+        isReadable = false,
+        quality = 1,
+    }
+end
