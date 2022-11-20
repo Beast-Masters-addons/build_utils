1c6311efa2e098b0c6c94a7f36ead06623454950
datagutten <datagutten@datagutten.net>
2022-11-20 11:44:05 +0100

    Add container API dummy for WoW Classic

---
+++ wow_api/container_classic.lua
@@ -0,0 +1,18 @@
+function GetContainerNumSlots(slot)
+    return 10
+end
+
+function GetContainerItemInfo(containerIndex, slotIndex)
+    return {
+        134414,
+        1,
+        false,
+        1,
+        false,
+        false,
+        "|cffffffff|Hitem:6948::::::::42:::::|h[Hearthstone]|h|r",
+        false,
+        true,
+        6948,
+    }
+end
