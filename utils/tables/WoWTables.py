6d700c1c9b4b5799a65473fad2fa6d48e3c4e22a
datagutten <datagutten@datagutten.net>
2023-03-19 13:07:50 +0100

    Update for wow.tools.local

--- utils/tables/WoWTables.py
+++ utils/tables/WoWTables.py
@@ -5,26 +5,45 @@ from . import WoWBuildUtils
 
 
 class WoWTables(WoWBuildUtils):
-    def __init__(self, data_folder=None, build_number='2.5.1.38537', locale=None, game='wrath'):
+    wow_tools_host = os.getenv('WOW_TOOLS', None) or 'http://127.0.0.1:5000'
+
+    def __init__(self, data_folder=None, build_number='3.4.1.48503', locale=None, game='wrath'):
         super().__init__(data_folder=data_folder)
         self.game = game
         self.build_number = build_number
         self.locale = locale
+        self.select_build(self.translate_build(game))
 
     @staticmethod
     def get_build(game_version=None):
         version = game_version or os.getenv('GAME_VERSION')
         if version == 'classic':
-            return '1.4.3.42926'
-        elif version == 'bcc':
-            return '2.5.4.42940'
+            return '1.14.3.46575'
         elif version == 'wrath':
-            return '3.4.0.45770'
+            return '3.4.1.48503'
         else:
-            return '9.2.5.42850'
+            return '10.0.5.48526'
+
+    @staticmethod
+    def translate_build(product):
+        if product == 'classic':
+            return 'wow_classic_era'
+        elif product == 'wrath':
+            return 'wow_classic'
+        elif product == 'retail':
+            return 'wow'
+
+    def get_builds(self):
+        url = '%s/casc/listManifests' % self.wow_tools_host
+        return self.get(url).json()
+
+    def select_build(self, product):
+        url = '%s/casc/switchProduct?product=%s' % (self.wow_tools_host, product)
+        response = self.get(url)
+        response.raise_for_status()
 
     def get_db_table(self, table):
-        url = 'https://wow.tools/dbc/api/export/?name=%s&build=%s' % (table, self.build_number)
+        url = '%s/dbc/export/?name=%s&build=%s' % (self.wow_tools_host, table, self.build_number)
         if self.locale:
             url += '&locale=%s' % self.locale
         response = self.get(url)
