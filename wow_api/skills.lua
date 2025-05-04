3f9f0a526240de58941d002512bfda7e5d64a66d
Anders Birkenes <datagutten@datagutten.net>
2025-05-04 09:16:35 +0200

    Add dummy for GetNumTradeSkills

--- wow_api/skills.lua
+++ wow_api/skills.lua
@@ -303,3 +303,7 @@ function GetSkillLineInfo(line)
     line = skillLines[line]
     return line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10], line[11], line[12]
 end
+
+function GetNumTradeSkills()
+    return 2
+end
