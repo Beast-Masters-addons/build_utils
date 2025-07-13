c749abbc7b0404f044b72f84fe4ba97f939c7fd8
Anders Birkenes <datagutten@datagutten.net>
2025-07-13 15:01:08 +0200

    Classic era now uses namespaced container API

--- wow_api/container.lua
+++ wow_api/container.lua
@@ -1,6 +1,6 @@
 local path = debug.getinfo(1).source:gsub('container.lua', ''):sub(2)
 
-if os.getenv('GAME_VERSION') == 'classic' or os.getenv('GAME_VERSION') == 'wrath' then
+if os.getenv('GAME_VERSION') == 'wrath' then
     loadfile(path .. 'container_classic.lua')()
 else
     loadfile(path .. 'container_namespace.lua')()
